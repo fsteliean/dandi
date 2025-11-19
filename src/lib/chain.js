241a4731368f15c3579d1db2d30438ce5308eb15
fsteliean <florin.steliean@yahoo.com>
2025-11-19 13:01:39 +0200

    update latest version

--- src/lib/chain.js
+++ src/lib/chain.js
@@ -1,7 +1,5 @@
 import { z } from "zod";
-import { StructuredOutputParser } from "@langchain/core/output_parsers";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { RunnableSequence } from "@langchain/core/runnables";
 
 // Strict output schema for summary
 const GithubSummarySchema = z.object({
@@ -11,15 +9,11 @@ const GithubSummarySchema = z.object({
     .describe("A list of 3-5 interesting or cool facts about the repository"),
 });
 
-const parser = StructuredOutputParser.fromZodSchema(GithubSummarySchema);
-
 const prompt = ChatPromptTemplate.fromMessages([
   [
     "system",
     "You will be given the content of a repository's README.md file. " +
-      "Generate a short summary (2-4 sentences) as well as a list of 3-5 interesting or cool facts. " +
-      "Respond strictly in the output JSON schema described below. " +
-      "{format_instructions}",
+      "Generate a short summary (2-4 sentences) as well as a list of 3-5 interesting or cool facts.",
   ],
   [
     "human",
@@ -27,15 +21,9 @@ const prompt = ChatPromptTemplate.fromMessages([
   ],
 ]);
 
-// Preload instructions (so you can await inside handler)
+// Bind structured output schema to model and create chain
 export async function getSummarizeGithubReadmeChain(llm) {
-  const partialedPrompt = await prompt.partial({
-    format_instructions: parser.getFormatInstructions(),
-  });
-  return RunnableSequence.from([
-    partialedPrompt,
-    llm,
-    parser,
-  ]);
+  const modelWithStructure = llm.withStructuredOutput(GithubSummarySchema);
+  return prompt.pipe(modelWithStructure);
 }
 
